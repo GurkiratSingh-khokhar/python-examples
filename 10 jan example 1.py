8dd746bbf694b69dbe6ee4771218b251d3ee8621
GurkiratSingh-khokhar <157706819+GurkiratSingh-khokhar@users.noreply.github.com>
2024-01-26 23:23:27 +0530

    corrected list and deleted empty file

--- 10 jan example 1.py
+++ 10 jan example 1.py
@@ -1,4 +1,4 @@
 print("staionary store")
 a=input('enter item')
-list1:["notebook","books","pen","pencil","eraser","Glue",NoteHolders,StickyTape,Scissors]
+list1:["notebook","books","pen","pencil","eraser","Glue","NoteHolders","StickyTape","Scissors"]
 
